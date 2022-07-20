d9350e46d2926038b7a67602d526de81c51b03ab
Melanie Degregorio <96491155+MelanieDegregorio23@users.noreply.github.com>
2022-07-19 22:24:20 -0300

    saque los numeros de los menus, hice backup de pagos que faltaba y termine el diseño de configuracion

--- configuracion.h
+++ configuracion.h
@@ -76,62 +76,180 @@ bool backupdepo(){
     if(pos==0)return false;
     return true;
 }
+bool backupPagos(){
+    FILE *p;
+
+    p=fopen("Pagos.bkp", "wb");
+    if(p==NULL) return false;
+    pagos aux;
+    int pos=0;
+    while(aux.LeerEnDisco(pos)==1){
+        fwrite(&aux, sizeof aux, 1, p);
+        pos++;
+    }
+    fclose(p);
+    if(pos==0)return false;
+    return true;
+}
 
 void menuConfiguracion()
 {
-
-    int opc;
+    const int ANCHO_MENU = 65;
+    const int ALTO_MENU = 14;
     bool estado = true;
-    while (estado==true)
-    {
-        system("cls");
-        cout<<" ________________________"<<endl<<endl;
-        cout<<"      MENU CONFIGURACION"<<endl;
-        cout<<" ________________________"<<endl<<endl;
-
-        cout<<" 1. COPIA DE SEGURIDAD DEL ARCHIVO DE SOCIOS "<<endl;
-        cout<<" 2. COPIA DE SEGURIDAD DEL ARCHIVO DE INSTRUCTORES "<<endl;
-        cout<<" 3. COPIA DE SEGURIDAD DEL ARCHIVO DE ARTICULOS "<<endl;
-        cout<<" 4. COPIA DE SEGURIDAD DEL ARCHIVO DE VENTAS"<<endl;
-        cout<<" 5. COPIA DE SEGURIDAD DEL ARCHIVO DE DEPORTES "<<endl;
-        cout<<" 0. VOLVER AL MENU PRINCIPAL"<<endl;
+     int key, opc, cursorX, cursorY;
+       while (estado==true){
+       cursorX=POSMENUX+13;
+       cursorY=POSMENUY + 4;
+      setBackgroundColor(COLOR_PANTALLA);
+      cls();
+      opc=0;
+      setColor(LETRA);
+      setBackgroundColor(FONDO);
+      recuadro(POSMENUX,POSMENUY, ANCHO_MENU,ALTO_MENU,LETRA,FONDO);
+      separadorH(POSMENUX,POSMENUY+2,ANCHO_MENU,LETRA,FONDO);
+      locate(POSMENUX+22,POSMENUY+1);
+        cout<<"MENU CONFIGURACION";
+        locate(POSMENUX+14,POSMENUY+4);
+        cout<<" COPIA DE SEGURIDAD DEL ARCHIVO DE SOCIOS."<<endl;
+        locate(POSMENUX+14,POSMENUY+5);
+        cout<<" COPIA DE SEGURIDAD DEL ARCHIVO DE INSTRUCTORES."<<endl;
+        locate(POSMENUX+14,POSMENUY+6);
+        cout<<" COPIA DE SEGURIDAD DEL ARCHIVO DE ARTICULOS."<<endl;
+        locate(POSMENUX+14,POSMENUY+7);
+        cout<<" COPIA DE SEGURIDAD DEL ARCHIVO DE VENTAS."<<endl;
+        locate(POSMENUX+14,POSMENUY+8);
+        cout<<" COPIA DE SEGURIDAD DEL ARCHIVO DE DEPORTES."<<endl;
+        locate(POSMENUX+14,POSMENUY+9);
+        cout<<" COPIA DE SEGURIDAD DEL ARCHIVO DE PAGOS."<<endl;
+        locate(POSMENUX+14,POSMENUY+10);
+        cout<<" VOLVER AL MENU PRINCIPAL."<<endl;
+        locate(POSMENUX+13,POSMENUY+11);
         cout<<endl;
-        cout<<" INGRESE LA OPCION DESEADA: ";
-        cin>>opc;
-        system("cls");
+         hidecursor();
+        locate(cursorX,cursorY);
+        cout<<">>";
+        key = getkey();
+        while(key != KEY_ENTER){
+        locate(cursorX,cursorY);
+        cout<<" ";
+        cout<<" ";
+        switch(key){
+        case KEY_DOWN:
+            if(opc < 6){
+                opc++;
+            }else{
+                opc=0;
+            }
+            break;
+        case KEY_UP:
+            if(opc > 6){
+                opc--;
+            }else{
+                opc=0;
+            }
+            break;
+        }
+        if(opc != 0){
+            cursorY = opc + POSMENUY + 4;
+        }else{
+            cursorY = POSMENUY + 4;
+        }
+        locate(cursorX,cursorY);
+        cout<<">>";
+        key = getkey();
+      }
+      setBackgroundColor(COLOR_PANTALLA);
+      cls();
+      showcursor();
         switch(opc)
         {
 
-        case 1:
+        case 0:
             system("cls");
-                if(backupsocio()) cout<<"COPIA DE SEGURIDAD DE SOCIOS REALIZADA"<<endl;
-                    else cout<<"NO SE PUDO HACER LA COPIA DE SEGURIDAD"<<endl;
+                separadorx(POSMENUX,POSMENUY+2,ANCHO_MENU,LETRA,FONDO);
+                locate(POSMENUX+25,POSMENUY+1);
+                cout<<"BACKUP SOCIO";
+                if(backupsocio()){
+                    gotoxy(46,6);
+                    cout<<"COPIA DE SEGURIDAD DE SOCIOS REALIZADA."<<endl;}
+                    else{
+                    gotoxy(46,6);
+                    cout<<"NO SE PUDO HACER LA COPIA DE SEGURIDAD."<<endl;}
+                gotoxy(46,9);
             system("pause");
             break;
-        case 2:  system("cls");
-                if(backupinstructores()) cout<<"COPIA DE SEGURIDAD  DE INSTRUCTORES REALIZADA"<<endl;
-                    else cout<<"NO SE PUDO HACER LA COPIA DE SEGURIDAD"<<endl;
+        case 1:  system("cls");
+                separadorx(POSMENUX,POSMENUY+2,ANCHO_MENU,LETRA,FONDO);
+                locate(POSMENUX+25,POSMENUY+1);
+                cout<<"BACKUP INSTRUCTORES";
+                if(backupinstructores()){
+                        gotoxy(46,6);
+                        cout<<"COPIA DE SEGURIDAD  DE INSTRUCTORES REALIZADA."<<endl;}
+                    else {
+                        gotoxy(46,6);
+                            cout<<"NO SE PUDO HACER LA COPIA DE SEGURIDAD."<<endl;}
+                gotoxy(46,9);
             system("pause");
             break;
-        case 3:system("cls");
-                if(backuparticulos()) cout<<"COPIA DE SEGURIDAD ARTICULOS REALIZADA"<<endl;
-                else cout<<"NO SE PUDO HACER LA COPIA DE SEGURIDAD"<<endl;
+        case 2:system("cls");
+                separadorx(POSMENUX,POSMENUY+2,ANCHO_MENU,LETRA,FONDO);
+                locate(POSMENUX+25,POSMENUY+1);
+                cout<<"BACKUP ARTICULOS";
+                if(backuparticulos()) {
+                        gotoxy(46,6);
+                    cout<<"COPIA DE SEGURIDAD ARTICULOS REALIZADA."<<endl;}
+                else {
+                    gotoxy(46,6);
+                cout<<"NO SE PUDO HACER LA COPIA DE SEGURIDAD."<<endl;}
+                gotoxy(46,9);
+                 system("pause");
+            break;
+        case 3:
+                system("cls");
+                separadorx(POSMENUX,POSMENUY+2,ANCHO_MENU,LETRA,FONDO);
+                locate(POSMENUX+25,POSMENUY+1);
+                cout<<"BACKUP VENTAS";
+                if( backupventas()){
+                        gotoxy(46,6);
+                    cout<<"COPIA DE SEGURIDAD DE VENTAS  REALIZADA."<<endl;}
+                else {
+                    gotoxy(46,6);
+                    cout<<"NO SE PUDO HACER LA COPIA DE SEGURIDAD."<<endl;}
+                gotoxy(46,9);
                  system("pause");
             break;
         case 4:
-                system("cls");
-                if( backupventas()) cout<<"COPIA DE SEGURIDAD DE VENTAS  REALIZADA"<<endl;
-                else cout<<"NO SE PUDO HACER LA COPIA DE SEGURIDAD"<<endl;
+               system("cls");
+                separadorx(POSMENUX,POSMENUY+2,ANCHO_MENU,LETRA,FONDO);
+                locate(POSMENUX+25,POSMENUY+1);
+                cout<<"BACKUP DEPORTES";
+                if(backupdepo()==true){
+                        gotoxy(46,6);
+                    cout<<"COPIA DE SEGURIDAD DE DEPORTES REALIZADA."<<endl;}
+
+                else {
+                        gotoxy(46,6);
+                cout<<"NO SE PUDO HACER LA COPIA DE SEGURIDAD."<<endl;}
+                gotoxy(46,9);
                  system("pause");
             break;
-        case 5:
-               system("cls");
-                if(backupdepo()==true) cout<<"COPIA DE SEGURIDAD DE DEPORTES REALIZADA"<<endl;
-                else cout<<"NO SE PUDO HACER LA COPIA DE SEGURIDAD"<<endl;
+        case 5:   system("cls");
+                separadorx(POSMENUX,POSMENUY+2,ANCHO_MENU,LETRA,FONDO);
+                locate(POSMENUX+24,POSMENUY+1);
+                cout<<"BACKUP PAGOS COUTA";
+                if(backupPagos()==true){
+                        gotoxy(46,6);
+                    cout<<"COPIA DE SEGURIDAD DE PAGOS REALIZADA."<<endl;}
+
+                else {
+                        gotoxy(46,6);
+                cout<<"NO SE PUDO HACER LA COPIA DE SEGURIDAD."<<endl;}
+                gotoxy(46,9);
                  system("pause");
             break;
 
-        case 0: estado=false;
+        case 6: estado=false;
             break;
 
         }
